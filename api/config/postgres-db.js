332b3075941558121dae860dae95e628788f7a18
minnecoder <sdparkin@protonmail.com>
2020-10-17 00:19:10 -0500

    Made postgres db file

---
+++ api/config/postgres-db.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize")
+
+module.exports = new Sequelize(
+    process.env.POSTGRES_DB,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PWD,
+    {
+        host: process.env.POSTGRES_SERVER,
+        dialect: "postgres"
+    }
+)
