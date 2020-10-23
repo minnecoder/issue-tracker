0f7f8ba501377b0293ba844d50271d9a6f6e2faa
minnecoder <sdparkin@protonmail.com>
2020-10-22 23:40:08 -0500

    Changes to file

--- api/config/postgres-db.js
+++ api/config/postgres-db.js
@@ -1,11 +1,11 @@
-const Sequelize = require("sequelize")
+const Sequelize = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_USER,
-    process.env.POSTGRES_PWD,
-    {
-        host: process.env.POSTGRES_SERVER,
-        dialect: "postgres"
-    }
-)
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PWD,
+  {
+    host: process.env.POSTGRES_SERVER,
+    dialect: 'postgres',
+  },
+);
