5e9c25b797017c46eb642fe63e8cd4e386566333
minnecoder <sdparkin@protonmail.com>
2020-10-27 00:11:03 -0500

    Added pages

---
+++ web/src/pages/ManageRoles.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../Header';
+import Sidebar from '../Sidebar';
+
+export default function ManageRoles() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <Main>
+        <h1>ManageRoles</h1>
+      </Main>
+    </div>
+  );
+}
+
+const Main = styled.div`
+position: fixed;
+right: 0;
+height: calc(100vh - 4rem);
+width: calc(100% - 200px);
+background: lightgray;
+`;
