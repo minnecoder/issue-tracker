54919ba3faaba49ef372c461fe5fe16c6f9c447c
minnecoder <sdparkin@protonmail.com>
2020-11-02 18:35:13 -0600

    Modified Tickets layout

--- web/src/pages/Tickets.js
+++ web/src/pages/Tickets.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import TicketTable from '../components/TicketTable';
+// import TicketTable from '../components/TicketTable';
 import Header from '../Header';
 import Sidebar from '../Sidebar';
+import TicketList from "../components/TicketList"
+import TicketDetails from "../components/TicketDetails"
 
 export default function Tickets() {
   return (
@@ -10,7 +12,21 @@ export default function Tickets() {
       <Header />
       <Sidebar />
       <Main>
-        <TicketTable />
+        <TicketArea>
+          <ListPanel>
+
+            <TableTitle>
+              <h3>Tickets</h3>
+              <button type="submit">Create A Ticket</button>
+            </TableTitle>
+            <TicketList /> 
+          </ListPanel>
+          <DetailPanel>
+          <TicketDetails />
+          </DetailPanel>
+          </TicketArea>
+    
+     
       </Main>
     </div>
   );
@@ -21,5 +37,38 @@ position: fixed;
 right: 0;
 height: calc(100vh - 4rem);
 width: calc(100% - 200px);
+display: flex;
 background: lightgray;
 `;
+
+const TicketArea = styled.div`
+display: flex;
+margin: 1rem;
+border: solid 1 px black;
+border-collapse: separate;
+border-radius: 5px;
+background: white;
+`
+
+
+const ListPanel = styled.div`
+flex: 1;
+height: 100vh;
+`
+
+const DetailPanel = styled.div`
+flex: 1;
+height: 100vh;
+border-left: solid 1px #ccc;
+`
+
+const TableTitle = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 1rem 1rem;
+border-bottom: solid 1px #ccc;
+
+button {
+  padding: .5rem;
+}
+`
