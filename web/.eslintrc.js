f6cc76a0e143dd9c9d01d416e71b0f795bab92f8
minnecoder <sdparkin@protonmail.com>
2020-11-08 22:09:07 -0600

    Fixed Eslint issues

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2020: true,
     },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'react/jsx-filename-extension': 0,
-    'linebreak-style': 0,
-    'react/prefer-stateless-function': 0,
-  },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        indent: ['error', 2],
+        'react/jsx-indent': ['error', 2],
+        'linebreak-style': 0,
+        'react/jsx-filename-extension': 0,
+    },
 };
