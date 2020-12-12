d529b5efb8549d769f75d1bb0ee0a1a3b7043551
minnecoder <sdparkin@protonmail.com>
2020-12-11 22:33:13 -0600

    Fixed typo

--- api/server.js
+++ api/server.js
@@ -32,4 +32,4 @@ app.use("/api/v1/tickethistorys", ticketHistory)
 
 
 const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.log(`Server us running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
