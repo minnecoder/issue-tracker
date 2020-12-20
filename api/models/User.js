6cedb6cf9530e9ebebfb0b12a7b0d7dcc8bf485e
minnecoder <sdparkin@protonmail.com>
2020-12-20 15:14:39 -0600

    made firstName and lastName required fields

--- api/models/User.js
+++ api/models/User.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
+    firstName: {
+        type: String,
+        required: [true, "First Name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last Name is required"]
+    },
     email: {
         type: String,
         required: [true, "Email is required"]
