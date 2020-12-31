bfb82c870436601ef99ff5f762743af2e962c9ef
minnecoder <sdparkin@protonmail.com>
2020-12-30 21:54:33 -0600

    Went from many styled items to wrapper and classes

--- web/src/components/TicketDetails.js
+++ web/src/components/TicketDetails.js
@@ -6,84 +6,101 @@ import { FaEdit } from 'react-icons/fa';
 export default function TicketDetails({ data, index }) {
 
   return (
-    <Main>
-      <TicketInfo key={index}>
-        <h2>{data.title}</h2>
-        <FaEdit />
-        <Info>
+    <Wrapper>
+      <div className="ticketInfo" key={index}>
+        <div className="ticketTitle">
+          <h2>{data.title}</h2>
+          <span>
+            <FaEdit style={{ marginTop: ".25rem" }} />
+          Edit
+          </span>
+        </div>
+        <div className="info">
           <div>
-            <InfoItem>
-              <p>Assigned Dev</p>
+            <section className="infoItem">
+              <p>Assigned To</p>
               <p>{data.assignedDev}</p>
-            </InfoItem>
-            <InfoItem>
+            </section>
+            <section className="infoItem">
               <p>Created By</p>
               <p>{data.submitter}</p>
-            </InfoItem>
+            </section>
 
           </div>
           <div>
-            <InfoItem>
+            <section className="infoItem">
               <p>Type</p>
               <p>{data.ticketType}</p>
-            </InfoItem>
-            <InfoItem>
+            </section>
+            <section className="infoItem">
               <p>Status</p>
               <p>{data.ticketStatus}</p>
-            </InfoItem>
-            <InfoItem>
+            </section>
+            <section className="infoItem">
               <p>Priority</p>
               <p>{data.ticketPriority}</p>
-            </InfoItem>
+            </section>
           </div>
-        </Info>
-        <Description>{data.description}</Description>
-      </TicketInfo>
+        </div>
+        <p className="description">{data.description}</p>
+      </div>
       <TicketNotes />
-      <TicketComment>
+      <div className="ticketComment">
         <textarea name="" id="" cols="30" rows="10" />
         <button type="submit">Submit</button>
-      </TicketComment>
-    </Main>
-  )
+      </div>
+    </Wrapper>)
 }
 
-const TicketInfo = styled.div` 
-margin: .5rem;
-h2{
-padding: 0.5rem 0;
+
+
+const Wrapper = styled.div`
+background: white;
+border-left: 1px solid #ccc;
+
+.ticketTitle {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h2{
+    margin-left: .25rem;
+  }
+  span{
+    
+    margin-right: 2rem; 
+    padding: .5rem;
+    color: white;
+    background: #EB7012;
+    border-radius: 5px;
+  }
 }
-`;
 
-const Description = styled.p` 
-padding-top: .5rem;
-`;
-
-const Info = styled.div`
-display: flex;
-justify-content: space-between;
-border-bottom: solid 1px #ddd;
-margin: 0 1rem;
-`;
-
-const InfoItem = styled.section`
-margin: .5rem 0;
+.description{
+  padding-top: .5rem;
+}
+.infoItem{
+  margin: .5rem 0;
  p:nth-child(1) {
    text-transform: uppercase;
    color: #ABB1B6;
    font-weight: 500;
    padding-bottom: .25rem;
  }
-`;
+}
+.info{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: solid 1px #ddd;
+  margin: 0 1rem;
+}
+.ticketInfo{
+  margin: .5rem;
+  h2{
+  padding: 0.5rem 0;
+  }
+}
 
-
-const Main = styled.div`
-background: white;
-border-left: 1px solid #ccc;
-`
-
-
-const TicketComment = styled.div`
+.ticketComment {
   display: flex;
   flex-direction: column;
   width: 40rem;
@@ -100,11 +117,12 @@ textarea {
 }
 
 button {
-margin-top: .5rem;
-padding: .5rem;
-width: 6rem;
-background: #EB7012;
-color: white;
-border-radius: 5px;
+  margin-top: .5rem;
+  padding: .5rem;
+  width: 6rem;
+  background: #EB7012;
+  color: white;
+  border-radius: 5px;
 }
-`;
+}
+`
