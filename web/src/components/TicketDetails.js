72e68ee5eaeefc7f8f3f849fb95026675a75225d
minnecoder <sdparkin@protonmail.com>
2021-01-01 09:39:36 -0600

    Ability to edit ticket type, priority and status

--- web/src/components/TicketDetails.js
+++ web/src/components/TicketDetails.js
@@ -1,16 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TicketNotes from "./TicketNotes"
 import styled from "styled-components"
 import { FaEdit } from 'react-icons/fa';
 
 export default function TicketDetails({ data, index }) {
+  const [isEdited, setIsEdited] = useState(false)
+  const [state, setState] = useState({
+    ticketPriority: "",
+    ticketType: "",
+    ticketStatus: ""
+  })
 
+  function handleChange(event) {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const arr = []
+    const firstName = sessionStorage.getItem("firstName")
+    const lastName = sessionStorage.getItem("lastName")
+    const fullName = `${firstName} ${lastName}`
+    // verify if each input is different
+    // if an input is different add object of data to an array
+    if (data.ticketType !== state.ticketType) {
+      arr.push({
+        user: fullName,
+        propertyChanged: "Ticket Type",
+        oldValue: data.ticketType,
+        newValue: state.ticketType
+      })
+    }
+    if (data.ticketStatus !== state.ticketStatus) {
+      arr.push({
+        user: fullName,
+        propertyChanged: "Ticket Status",
+        oldValue: data.ticketStatus,
+        newValue: state.ticketStatus
+      })
+    }
+    if (data.ticketPriority !== state.ticketPriority) {
+      arr.push({
+        user: fullName,
+        propertyChanged: "Ticket Priority",
+        oldValue: data.ticketPriority,
+        newValue: state.ticketPriority
+      })
+    }
+    console.log(arr)
+    // object must have user name, property changed(i.e. Ticket Status), old value, and new value
+    // iterate through array of objects and send each item to API end point
+    for (let i = 0; i < arr.length; i++) {
+      await fetch("/api/v1/tickethistorys", {
+        method: "POST",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          user: arr[i].user,
+          propertyChanged: arr[i].propertyChanged,
+          oldValue: arr[i].oldValue,
+          newValue: arr[i].newValue
+        })
+      })
+    }
+    // after complettion set isEdited to false
+    setIsEdited(false)
+
+  }
   return (
     <Wrapper>
       <div className="ticketInfo" key={index}>
         <div className="ticketTitle">
           <h2>{data.title}</h2>
-          <span>
+          <span onClick={() => setIsEdited(true)}>
             <FaEdit style={{ marginTop: ".25rem" }} />
           Edit
           </span>
@@ -25,31 +90,83 @@ export default function TicketDetails({ data, index }) {
               <p>Created By</p>
               <p>{data.submitter}</p>
             </section>
+          </div>
 
-          </div>
-          <div>
-            <section className="infoItem">
-              <p>Type</p>
-              <p>{data.ticketType}</p>
-            </section>
-            <section className="infoItem">
-              <p>Status</p>
-              <p>{data.ticketStatus}</p>
-            </section>
-            <section className="infoItem">
-              <p>Priority</p>
-              <p>{data.ticketPriority}</p>
-            </section>
-          </div>
+          {isEdited ?
+            <div>
+              <section className="infoItem">
+                <p>Type</p>
+                <select
+                  name="ticketType"
+                  value={state.ticketType}
+                  onChange={handleChange}
+                >
+                  <option value="">-</option>
+                  <option value="Bug">Bug</option>
+                  <option value="New Feature">New Feature</option>
+                  <option value="Improvement">Improvement</option>
+                  <option value="Task">Task</option>
+                  <option value="Testing">Testing</option>
+                </select>
+              </section>
+              <section className="infoItem">
+                <p>Status</p>
+                <select
+                  name="ticketStatus"
+                  value={state.ticketStatus}
+                  onChange={handleChange}
+                >
+                  <option value="">-</option>
+                  <option value="Open">Open</option>
+                  <option value="Resolved">Resolved</option>
+                  <option value="Closed">Closed</option>
+                  <option value="Reopened">Reopened</option>
+                  <option value="In Progress">In Progress</option>
+                </select>
+              </section>
+              <section className="infoItem">
+                <p>Priority</p>
+                <select
+                  name="ticketPriority"
+                  value={state.ticketPriority}
+                  onChange={handleChange}
+                >
+                  <option value="">-</option>
+                  <option value="Lowest">Lowest</option>
+                  <option value="Low">Low</option>
+                  <option value="Medium">Medium</option>
+                  <option value="High">High</option>
+                  <option value="Highest">Highest</option>
+                </select>
+              </section>
+              <button onClick={handleSubmit} className="submitBtn">Submit</button>
+            </div>
+            :
+            <div>
+              <section className="infoItem">
+                <p>Type</p>
+                <p>{data.ticketType}</p>
+              </section>
+              <section className="infoItem">
+                <p>Status</p>
+                <p>{data.ticketStatus}</p>
+              </section>
+              <section className="infoItem">
+                <p>Priority</p>
+                <p>{data.ticketPriority}</p>
+              </section>
+            </div>
+          }
         </div>
+
         <p className="description">{data.description}</p>
-      </div>
+      </div >
       <TicketNotes />
       <div className="ticketComment">
         <textarea name="" id="" cols="30" rows="10" />
         <button type="submit">Submit</button>
       </div>
-    </Wrapper>)
+    </Wrapper >)
 }
 
 
@@ -66,7 +183,7 @@ border-left: 1px solid #ccc;
     margin-left: .25rem;
   }
   span{
-    
+    cursor: pointer;
     margin-right: 2rem; 
     padding: .5rem;
     color: white;
@@ -79,7 +196,9 @@ border-left: 1px solid #ccc;
   padding-top: .5rem;
 }
 .infoItem{
-  margin: .5rem 0;
+  ${'' /* margin: .5rem 0; */}
+  padding: .5rem;
+  
  p:nth-child(1) {
    text-transform: uppercase;
    color: #ABB1B6;
@@ -89,9 +208,25 @@ border-left: 1px solid #ccc;
 }
 .info{
   display: flex;
+  flex-direction: column-reverse;
   justify-content: space-between;
   border-bottom: solid 1px #ddd;
   margin: 0 1rem;
+  div{
+    display: flex;
+    justify-content: space-between;
+  }
+  select{
+    border: 1px solid black;
+  }
+  .submitBtn{
+    width: 8rem;
+    padding: .5rem;
+    margin: .5rem;
+    background: #EB7012;
+    color: white;
+    border-radius: 5px;
+}
 }
 .ticketInfo{
   margin: .5rem;
